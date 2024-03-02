9a3890e8939f5afe07b4853034fc7695e0b89a6b
ArtemVaska <artem_vasilev_01@list.ru>
2024-03-02 07:23:26 +0300

    Add 2 R scripts for data processing

---
+++ r_scripts/draw-heatmap.r
@@ -0,0 +1,44 @@
+library(gplots)
+
+# Read normalized counts
+data = read.table("stdin", header=T, sep="\t", as.is=TRUE)
+
+gene = data[,1]
+vals = as.matrix(data[,2:ncol(data)])
+
+# Adds a little noise to each element
+# To avoid the clusteing function failing on zero
+# variance datalines.
+vals = jitter(vals, factor = 1, amount=0.00001)
+
+
+# Calculate zscore
+score = NULL
+for (i in 1:nrow(vals)) {
+    row=vals[i,]
+    zscore=(row-mean(row))/sd(row)
+    score =rbind(score,zscore)
+}
+
+row.names(score) = gene
+zscore=score
+
+# Generate heatmap
+mat = as.matrix(zscore)
+
+# Opent the drawing device.
+pdf('results/heatmaps/output.pdf')
+
+colors = colorRampPalette(c("green","black","red"),space="rgb")(256)
+heatmap.2(
+    mat,
+    col = colors,
+    density.info = "none",
+    trace = "none",
+    margins = c(14, 14),
+    lhei = c(1, 5),
+    cexRow = 0.2,  # Adjust the font size for rows
+    cexCol = 0.6   # Adjust the font size for columns
+)
+
+invisible(dev.off())
